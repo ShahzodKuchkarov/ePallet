40ce1fc1879a81a98315c87e086433f662f5b86d
shahzodkuchkarov <shahzodk0@gmail.com>
2021-06-17 10:54:57 +0400

    TEST

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React IN ONE MINUT, BUT IT IS IMPOSSIBLE
         </a>
       </header>
     </div>
